02d73a18b56c8ea018e32d1e402581624ecbf9ce
MegumiFushiguroo <147046731+MegumiFushiguroo@users.noreply.github.com>
2024-01-20 20:16:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,21 +7,22 @@ startText = """
 Bot by:- @LeviAckerman1709.
 Credit:- @LexicaAPI.
 Commands:
-`/draw`: create images
-`/upscale`: upscale your images
-`/gpt`: chatgpt
-`/bard`: bard ai by google
-`/mistral`: mistral ai
-`/llama`: llama by meta ai
-`/palm`: palm by google
-`/reverse`: reverse image search
-`/gemini`: gemini by google
+/draw: create images
+/upscale: upscale your images
+/gpt: chatgpt
+/bard: bard ai by google
+/mistral: mistral ai
+/llama: llama by meta ai
+/palm: palm by google
+/reverse: reverse image search
+/gemini: gemini by google
 """
 
 @Client.on_message(filters.command(["start","help","repo","source"]))
 async def start(_: Client, m: t.Message):
-    await m.reply_text(
-        startText,
+    await = m.reply_photo(
+        photo = “https://telegra.ph/file/d0d0a7f81665ff884a59b.jpg” , 
+        caption = startText,
         reply_markup=t.InlineKeyboardMarkup(
             [
                 [
