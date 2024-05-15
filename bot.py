473d370af7a806258e5220717ab71fb50f450b84
MegumiFushiguroo <147046731+MegumiFushiguroo@users.noreply.github.com>
2024-05-15 21:42:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,6 @@ class Bot(Client):
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
-            support_chat=Config.SUPPORT_CHAT,
             plugins=dict(root="plugins"),
         )
     async def start(self):
