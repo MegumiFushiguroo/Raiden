5d1fc0460e7733c6cdff86407adadcb98abfaacb
MegumiFushiguroo <147046731+MegumiFushiguroo@users.noreply.github.com>
2024-05-15 21:24:24 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -37,3 +37,28 @@ async def askAboutImage(_:Client,m:t.Message,mediaFiles: list,prompt:str):
         images.append(image)
     output = await geminiVision(prompt if prompt else "whats this?","geminiVision",images)
     await m.reply_text(output)
+
+
+@Client.on_message(filters.command(["assis"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+async def chat(app :app, message):
+
+    try:
+        start_time = time.time()
+        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+        if len(message.command) < 2:
+            await message.reply_text(
+            "**Hello! How can I assist you today?**")
+        else:
+            a = message.text.split(' ', 1)[1]
+            MODEL = "gpt-3.5-turbo"
+            resp = openai.ChatCompletion.create(model=MODEL,messages=[{"role": "user", "content": a}],
+    temperature=0.2)
+            x=resp['choices'][0]["message"]["content"]
+            text = x    
+            tts = gTTS(text, lang='en')
+            tts.save('output.mp3')
+            await app.send_voice(chat_id=message.chat.id, voice='output.mp3')
+            os.remove('output.mp3')            
+
+    except Exception as e:
+        await message.reply_text(f"**Error**: {e} ") 
